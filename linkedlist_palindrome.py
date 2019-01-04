fa4d45ff2969dfbe93ae5357b1ee2b64cb0619c7
tongtong <ly_tongtong@163.com>
2019-01-04 21:17:50 +0800

    判断回文字符串

--- linkedlist_palindrome.py
+++ linkedlist_palindrome.py
@@ -1,6 +1,5 @@
 '''
 判断是否是回文字符串
-dd
 '''
 
 def reverse(slow):
@@ -20,22 +19,22 @@ def is_palindrome(l):
         fast = fast.next.next
         slow = slow.next
 
-	end_node = reverse(slow)
-	start_node = l.__head
-	is_palin = True
-	while start_node and end_node:
-		if start_node.data == end_node.data:
-			start_node = start_node.next
-			end_node = end_node.next
-		else:
-			is_palin = False
-			break
-	return is_palin
+    end_node = reverse(slow)
+    start_node = l.__head
+    is_palin = True
+    while start_node and end_node:
+        if start_node.data == end_node.data:
+            start_node = start_node.next
+            end_node = end_node.next
+        else:
+            is_palin = False
+            break
+    return is_palin
 
 if __name__=='__main__':
-	test_str_arr = ['ab', 'aa', 'aba', 'abba', 'abcba']
-	for str in test_str_arr:
-		l = SinglyLinkedList()
-		for i in str:
-			l.insert_to_head(i)
-		print(is_palindrome(l))
+    test_str_arr = ['ab', 'aa', 'aba', 'abba', 'abcba']
+    for str in test_str_arr:
+        l = SinglyLinkedList()
+        for i in str:
+            l.insert_to_head(i)
+        print(is_palindrome(l))
