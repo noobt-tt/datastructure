b8dba6eaac82af43a5c169604ec50aa3af14ab9e
tongtong <ly_tongtong@163.com>
2019-01-11 16:16:49 +0800

    test

--- queue/dynamic_array_queue.py
+++ queue/dynamic_array_queue.py
@@ -29,15 +29,15 @@ class DynamicArrayQueue:
 	def __repr__(self) -> str:
 		return "".join(item for item in self._items[self._head:self._tail])
 
-	if __name__ == '__main__':
-		q = DynamicArrayQueue()
-		for i in range(10):
-			q.enqueue(str(i))
-		print(q)
-		for _ in range(3):
-			q.dequeue()
-		print(q)
+if __name__ == '__main__':
+	q = DynamicArrayQueue(10)
+	for i in range(10):
+		q.enqueue(str(i))
+	print(q)
+	for _ in range(3):
+		q.dequeue()
+	print(q)
 
-		q.enqueue("7")
-		q.enqueue("8")
-		print(q)
+	q.enqueue("7")
+	q.enqueue("8")
+	print(q)
